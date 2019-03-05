f9348262b6ac37619f5e4c56137ef7102199cc13
Ryan Burnette <ryan.burnette@gmail.com>
2019-03-05 13:52:40 -0500

    M  index.js M  package.json M  readme.md

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (req,res,next) {
   if (!['staging','production'].includes(process.env.NODE_ENV)) {
     return next();
   }
-  if (req.protocol === 'https') {
+  if ((req.headers['x-forwarded-proto'] || req.protocol) === 'https') {
     return next();
   }
   res.redirect('https://'+req.headers.host+req.url);
--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 # @ryanburnette/express-force-https
 
-Force HTTPS in Express on staging and production environments.
+Force HTTPS in Express on staging and production environments. Takes into
+account that the app may be running behind a proxy or on Heroku.
 
 ```javascript
 var express = require('express');
