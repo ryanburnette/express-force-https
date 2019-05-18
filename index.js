54154ff6b16c48e4c66e69df7be788c25c0c0141
Ryan Burnette <ryan.burnette@gmail.com>
2019-05-18 07:11:55 -0400

    1.4.3

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 module.exports = function (req, res, next) {
   var env = process.env.NODE_ENV;
-  if (env != 'production') { return next(); }
+  if (env !== 'production') { return next(); }
   var proto = req.headers['x-forwarded-proto'] || req.protocol;
-  if (proto === 'https') { return next(); }
-  res.redirect('https://'+req.headers.host+req.url);
+  if (proto !== 'https') { return res.redirect('https://'+req.headers.host+req.url); }
+  next();
 };
